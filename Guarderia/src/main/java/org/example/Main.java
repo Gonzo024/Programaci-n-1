979ea172330086d461b9d62a30bfa3bc829ca2e0
Gonzo024 <santiagofutbol2007@gmail.com>
2025-02-27 01:25:21 -0500

    agregando guarderia como carpeta normal

---
+++ Guarderia/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import javax.swing.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        String nombre = JOptionPane.showInputDialog("Ingrese el nombre del usuario");
+        int edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el edad del usuario"));
+        String genero = JOptionPane.showInputDialog("Ingrese el genero del usuario");
+        String id = JOptionPane.showInputDialog("Ingrese el id del usuario");
+        String alergias = JOptionPane.showInputDialog("Ingrese alergias del usuario");
+        String nombreAcudiengte =JOptionPane.showInputDialog("Ingrese el nombre del usuario");
+        String telefono = JOptionPane.showInputDialog("Ingrese el telefono del usuario");
+
+    }
+}
